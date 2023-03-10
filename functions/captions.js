76a086354a7918d063a07b305c36e283f7406450
bfasenfest <aowbackup@gmail.com>
2023-03-10 01:04:35 -0500

    try npm package

--- functions/captions.js
+++ functions/captions.js
@@ -1,12 +1,13 @@
 const { spawn } = require('child_process');
 const fs = require('fs');
+import { getSubtitles } from 'youtube-captions-scraper';
 
 const YTDlpWrap = require('yt-dlp-wrap').default;
 
 
 exports.handler = function(event, context, callback) {
   const videoId = event.queryStringParameters.videoId;
-  const ytDlpPath = `yt-dlp`;
+  const ytDlpPath = `${__dirname}/yt-dlp`;
   const filePath = `tmp/captions-${videoId}`;
   const args = [
     '--write-auto-sub',
@@ -17,6 +18,13 @@ exports.handler = function(event, context, callback) {
   ];
 
   console.log(ytDlpPath)
+
+  getSubtitles({
+    videoID: videoId , // youtube video id
+    lang: 'en' // default: `en`
+  }).then(captions => {
+    console.log(captions);
+  });
 
   // await YTDlpWrap.downloadFromGithub(
   //   'bin/yt-dlp',
@@ -58,30 +66,30 @@ exports.handler = function(event, context, callback) {
   //   })
   // }
 
-  const process = spawn( ytDlpPath, args, { cwd: '.' });
+  // const process = spawn( ytDlpPath, args, { cwd: '.' });
 
-  process.on('exit', (code) => {
-    if (code !== 0) {
-      console.error(`yt-dlp process exited with code ${code}`);
-      return callback(null, {
-        statusCode: 500,
-        body: JSON.stringify({
-          error: `yt-dlp process exited with code ${code}`
-        })
-      });
-    }
+  // process.on('exit', (code) => {
+  //   if (code !== 0) {
+  //     console.error(`yt-dlp process exited with code ${code}`);
+  //     return callback(null, {
+  //       statusCode: 500,
+  //       body: JSON.stringify({
+  //         error: `yt-dlp process exited with code ${code}`
+  //       })
+  //     });
+  //   }
 
-    const file = fs.readFileSync(filePath + '.en-en-US.vtt');
-    fs.unlinkSync(filePath + '.en-en-US.vtt');
+  //   const file = fs.readFileSync(filePath + '.en-en-US.vtt');
+  //   fs.unlinkSync(filePath + '.en-en-US.vtt');
 
-    return callback(null, {
-      statusCode: 200,
-      headers: {
-        'Content-Type': 'text/vtt',
-        'Content-Disposition': `attachment; filename="${videoId}.vtt"`
-      },
-      body: file.toString('base64'),
-      isBase64Encoded: true
-    });
-  });
+  //   return callback(null, {
+  //     statusCode: 200,
+  //     headers: {
+  //       'Content-Type': 'text/vtt',
+  //       'Content-Disposition': `attachment; filename="${videoId}.vtt"`
+  //     },
+  //     body: file.toString('base64'),
+  //     isBase64Encoded: true
+  //   });
+  // });
 };
